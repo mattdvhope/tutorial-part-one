d0c8d4bc7fab69a415f5da97be741ad7df50ae7a
mattdvhope <mattagape@me.com>
2018-04-26 10:49:50 -0400

    Edit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,7 +3,7 @@ import Link from "gatsby-link";
 
 export default () => (
   <div>
-    <p>Hello world from my second Gatsby page</p>
+    <p>Hello world from my second Gatsby page from Bangkok</p>
     <Link to="/">back home</Link>
   </div>
 );
